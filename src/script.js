d022c135304fbb5f9fa5390e78ad62b114323850
Bradley Peterson <bradlypetrsn1991@gmail.com>
2019-10-17 10:19:19 -0500

    Adding buttons and setting grid layout.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
+const container = document.querySelector('.container');
+initButtons();
+
+
 function add(a,b) {
   return a+b;
 }
@@ -27,5 +31,19 @@ function operate(operator,a,b) {
       break;
     case '*':
       return multiply(a,b);
+  }
+}
+
+function initButtons() {
+  const btnContainer = document.querySelector('.btnContainer');
+  const btnLabels = ['C', '/', '*', '-', '+', '=', '.', 7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+  const btnClasses = ['C', 'divide', 'multiply', 'subtract','add','equals','decimal', 7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
+  for(i=0;i<17;i++) {
+    var btn = document.createElement("BUTTON");
+    if(i<btnLabels.length) {
+      btn.className = `b${btnClasses[i]}`;
+      btn.innerHTML = btnLabels[i];
+    }
+    btnContainer.appendChild(btn);
   }
 }
